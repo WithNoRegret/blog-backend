8b6c8501dec528eb3a463da4b8ee8919ee5b7f41
Sergey Pjankov <no.regret.ccg@gmail.com>
2024-05-27 17:14:40 +0300

    added add post feature

--- src/index.js
+++ src/index.js
@@ -16,4 +16,12 @@ app.get('/posts-list', (req, res) => {
     res.send(obj);
 });
 
+app.post('/add-post', (req, res) => {
+    const obj = JSON.parse(fs.readFileSync('./public/db.json', 'utf8'));
+    obj.results += 1;
+    obj.posts.push(req.body);
+    fs.writeFileSync('./public/db.json', JSON.stringify(obj));
+    res.send(obj);
+});
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
